35109cd0381bc22d044fefdcdbadb7ef3b094c4e
ramadhaniwanjenja <r.wanjenja@alustudent.com>
2025-07-07 00:34:38 +0200

    main entry point for a Flutter notes

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,7 +1,7 @@
+// This file implements the main entry point for a Flutter notes app using Firebase
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:provider/provider.dart' show ChangeNotifierProvider;
 import 'providers/auth_provider.dart';
 import 'providers/notes_provider.dart';
 import 'screens/auth_screen.dart';
